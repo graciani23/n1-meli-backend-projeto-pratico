1edf3bc8e70b4a2570d5b0d0c675f23c8b4e45d2
graciani23 <gracianigss@gmail.com>
2019-10-24 13:12:28 -0300

    criação de nova rota

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -25,3 +25,20 @@ exports.getNomeId = (req, res) => {
     const nomeFunc = nomeId.nomeColaborador
     res.status(200).send(nomeFunc)
 }
+
+function FormataStringData(data) {
+    var dia  = data.split("/")[0];
+    var mes  = data.split("/")[1];
+    var ano  = data.split("/")[2];
+  
+    return ano + '-' + ("0"+mes).slice(-2) + '-' + ("0"+dia).slice(-2);
+    // Utilizo o .slice(-2) para garantir o formato com 2 digitos.
+  }
+
+exports.getOrdem = (req, res) => {
+    const ordenados = tarefas.sort(function(a, b) {
+        return FormataStringData(a.dataInclusao) - FormataStringData(b.dataInclusao)
+    })
+    res.status(200).send(ordenados)
+}
+
